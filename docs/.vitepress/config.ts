a111f57c037294b42f65009e4ba8427af065236b
Ruslan Kokoev <ruslan.kokoev.1999@gmail.com>
2026-06-09 00:38:12 +0300

    scaffold

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,33 @@
+import {defineConfig} from 'vitepress'
+import {withMermaid} from 'vitepress-plugin-mermaid'
+
+export default withMermaid(defineConfig({
+  title: 'Ruslan Kokoev CV',
+  description: 'Ruslan Kokoev CV',
+  base: '/cv/',
+
+  mermaid: {
+    flowchart: {
+      useMaxWidth: false,
+      htmlLabels: false,
+      padding: 15,
+    },
+  },
+  sitemap: {
+    hostname: 'https://thumbrise.github.io/cv/',
+  },
+  head: [
+    ['link', {rel: 'icon', type: 'image/svg+xml', href: '/cv/favicon.svg'}],
+    ['link', {rel: 'icon', type: 'image/png', sizes: '96x96', href: '/cv/favicon-96x96.png'}],
+    ['link', {rel: 'apple-touch-icon', sizes: '180x180', href: '/cv/apple-touch-icon.png'}],
+    ['link', {rel: 'manifest', href: '/cv/site.webmanifest'}],
+  ],
+
+  themeConfig: {
+    nav: [],
+    sidebar: {},
+    socialLinks: [
+      {icon: 'github', link: 'https://github.com/thumbrise'},
+    ],
+  },
+}))
